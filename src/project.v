2228f5a79c160fb6580725e6e95ceb2e162a86a4
Nandanashibu <133033772+Nandanashibu@users.noreply.github.com>
2025-03-25 21:31:17 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_Nandanashibu_multiplier-project (  // Fixed: Hyphen -> underscore (recommended for Verilog)
+module tt_um_multiplier-project (  // Fixed: Hyphen -> underscore (recommended for Verilog)
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
